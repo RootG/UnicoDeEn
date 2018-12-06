d98761be2a8a4e7ab7b67a660b4cc2a49ce275ce
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-12-06 16:10:17 +0300

    Added Input and Output fields to index.html.

---
+++ src/main/resources/public/javascripts/autoresize.js
@@ -0,0 +1,8 @@
+$(window).on('load', function () {
+    $('textarea').each(function () {
+        this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+    }).on('input', function () {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+});
