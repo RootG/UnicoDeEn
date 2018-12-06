df00f9031e8b73c73c156c97c9104248455da397
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-12-06 17:16:09 +0300

    Added Procfile for Heroku.

--- src/main/java/com/github/rootg/unicodeen/UnicoDeEn.java
+++ src/main/java/com/github/rootg/unicodeen/UnicoDeEn.java
@@ -5,12 +5,17 @@ import spark.Service;
 import java.util.Date;
 
 public class UnicoDeEn {
+    private static final String PORT = System.getenv("PORT");
+    private static final int EXPIRE_TIME_SECONDS = 600;
+
     public static void main(String[] args) {
         Service service = Service.ignite();
-        int expireTimeSeconds = 600;
-        service.staticFiles.header("Cache-Control", "public, max-age=" + expireTimeSeconds);
+        if (System.getenv("PORT") != null) {
+            service.port(Integer.parseInt(PORT));
+        }
+        service.staticFiles.header("Cache-Control", "public, max-age=" + EXPIRE_TIME_SECONDS);
         service.staticFiles.header("Expires",
-                new Date(System.currentTimeMillis() + expireTimeSeconds * 1000).toString());
+                new Date(System.currentTimeMillis() + EXPIRE_TIME_SECONDS * 1000).toString());
         service.staticFileLocation("public");
         service.init();
     }
