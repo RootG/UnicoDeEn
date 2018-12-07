14a179c25057184f431fda20d6cfea50a7e31e1e
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-12-07 18:20:34 +0300

    Added UTF16 encoding from input to output for non printible characters.

--- src/main/resources/public/javascripts/input.js
+++ src/main/resources/public/javascripts/input.js
@@ -1,12 +1,15 @@
 $(window).on('load', function () {
     $('#input').bind('input propertychange', function () {
         var output = $('#output');
-        output.val('');
+        output.val('').trigger('input');
         var text = $(this).val();
         for (var i = 0; i < text.length; i++) {
             var c = text.charAt(i);
             if (c >= ' ' && c <= '~') {
-                output.val(output.val() + c);
+                output.val(output.val() + c).trigger('input');
+            } else {
+                output.val(output.val() + '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4))
+                    .trigger('input');
             }
         }
     });
