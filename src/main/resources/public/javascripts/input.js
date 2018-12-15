728ab0285e7fe90504e75247e612f4f58d18dc2d
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-12-15 14:13:16 +0300

    Improved encoding time.

--- src/main/resources/public/javascripts/input.js
+++ src/main/resources/public/javascripts/input.js
@@ -1,16 +1,15 @@
 $(window).on('load', function () {
     $('#input').bind('input propertychange', function () {
-        var output = $('#output');
-        output.val('').trigger('input');
+        var output = '';
         var text = $(this).val();
         for (var i = 0; i < text.length; i++) {
             var c = text.charAt(i);
             if (c >= ' ' && c <= '~') {
-                output.val(output.val() + c).trigger('input');
+                output += c;
             } else {
-                output.val(output.val() + '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4))
-                    .trigger('input');
+                output += '\\u' + ('0000' + c.charCodeAt(0).toString(16)).slice(-4);
             }
         }
+        $('#output').val(output).trigger('input');
     });
 });
